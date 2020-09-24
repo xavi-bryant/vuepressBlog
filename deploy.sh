23d4bc4dbba9d009637dc8c822bbb12bc7149dae
xavier <420948614@qq.com>
2020-09-24 22:16:43 +0800

    action 1

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
 # 生成静态文件
 npm run build
 
@@ -20,18 +25,3 @@ git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 #git push -f git@github.com:miluluyo/vdoingBlog.git master:gh-pages
-
-
-# deploy to coding
-#echo 'www.miluluu.com\miluluu.com' > CNAME  # 自定义域名
-#if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#  codingUrl=git@e.coding.net:miluluu/miluluu.git
-#else
-#  codingUrl=https://miluluu:${CODING_TOKEN}@e.coding.net/miluluu/miluluu/miluluu.git
-#fi
-#git add -A
-#git commit -m "${msg}"
-#git push -f $codingUrl master # 推送到coding
-
-cd - # 退回开始所在目录
-rm -rf docs/.vuepress/dist
