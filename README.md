b6206ecc7222134decdbca2a7b8c538cbb837669
xavier <420948614@qq.com>
2020-09-24 21:53:57 +0800

    del

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 1. 项目地址：https://github.com/xavi-bryant/vuepressBlog
 
-2. 博客地址：https://www.xavierxin.com/
-3. 
-4. 
--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ if [ -z "$VDOING" ]; then
   githubUrl=git@github.com:xavi-bryant/vuepressBlog.git
 else
   msg='来自 github actions的自动部署'
-  githubUrl=https://xavi-bryant:${VDOING}@github.com:xavi-bryant/vuepressBlog.git
+  githubUrl=ssh://xavi-bryant:${VDOING}@github.com:xavi-bryant/vuepressBlog.git
   git config --global user.name "xavi-bryant"
   git config --global user.email "420948614@qq.com"
 fi
